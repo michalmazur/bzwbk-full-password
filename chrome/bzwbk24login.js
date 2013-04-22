365654f65890da297d1585bfa24c085643452523
Michal Mazur <michal@michalmazur.com>
2013-04-22 18:34:15 +0200

    Added Chrome extension.

---
+++ chrome/bzwbk24login.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        BZ WBK Full Password
+// @description Allows you to enter your full password on BZ WBK login page.
+// @include     https://www.centrum24.pl/centrum24-web/?x=*
+// @version     1.0
+// ==/UserScript==
+
+/* 
+ * It is impossible to identify the login page by the URL alone, so check for
+ * the existence of the login form.
+ */
+if (document.getElementById('pinForm')) {
+	var main = function() {
+		$('#gotoold').css('margin-top', '15px'); // fix overlap
+
+		$('#pinDiv table tbody').append('<tr><td></td><td colspan=20>'
+			+ '<input type="password" id="full_password" maxlength="20" style="width: 100%;" />'
+			+ '</td><td></td></tr>');
+		$('#full_password').focus();
+
+		$('#full_password').bind('input', function(event) {
+			var password = event.target.value;
+			var input_boxes = $('#pinDiv tbody tr:first td input:visible');
+			for (var i = 0; i < input_boxes.length; i++) {
+				if (!input_boxes[i].disabled) {
+					input_boxes[i].value = password.substring(i, i + 1);
+				}
+			}
+		});
+	};
+
+	var script = document.createElement('script');
+	script.type = "text/javascript";
+	script.textContent = '(' + main.toString() + ')();';
+	document.body.appendChild(script);
+}
